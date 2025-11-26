f2c2797df639746567e1c44659e16db35cabb90a
barackwilliam <barackwilliam12@gmail.com>
2025-11-26 23:31:32 +0300

    descreption are now optional

--- ecom/models.py
+++ ecom/models.py
@@ -84,7 +84,7 @@ class Product(models.Model):
     )
     
     price = models.PositiveIntegerField()
-    description = models.CharField(max_length=500)
+    description = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.name
